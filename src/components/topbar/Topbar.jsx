66f24090d4e3c5e6ba1e3589f24f522b646ee3b7
Stuart <31868328+SuperBrava@users.noreply.github.com>
2022-02-16 15:55:25 +0000

    Initial commit
    Framework model

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./topbar.scss";
+
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className='Topbar'>
+      
+    </div>
+  );
+}
