f557d4846e581238a3492de013a9aa503fd4f1b6
Stuart <31868328+SuperBrava@users.noreply.github.com>
2022-02-18 16:07:38 +0000

    Update 2

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,31 @@
-import "./intro.scss"
+import "./intro.scss";
+
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  
+
   return (
-    <div className='intro'>
-        
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Stuart Judd</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
